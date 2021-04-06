342f6da0ba1b6dc6d23b9e92bdb6e19318bf2202
Milind Prajapat <milindprajapat@gmail.com>
2021-04-06 15:17:29 +0530

    Add Test Data

--- Mosaic.py
+++ Mosaic.py
@@ -1,6 +1,8 @@
+import os
 import cv2
 import numpy as np
 from keras.models import load_model
+import matplotlib.pyplot as plt
 
 model = load_model("best_val_loss.hdf5")
 
@@ -13,11 +15,28 @@ Labels = ["ka", "kha", "ga", "gha", "kna",
           "petchiryakha", "patalosaw", "ha", "chhya", "tra", "gya",
           "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-file = ""
+for file in os.listdir("Test"):
+    img = cv2.imread(f"Test/{file}")
 
-img = cv2.imread(file)
-img = cv2.resize(img, (32,32), interpolation = "nearest")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-x = img.reshape(-1, 32, 32, 1) / 255.0
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
+    morph = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+    
+    div = gray / morph
+    gray = np.array(cv2.normalize(div, div, 0, 255, cv2.NORM_MINMAX), np.uint8)
 
-print(Label[np.argmax(model.predict([x]))])
+    blur = cv2.medianBlur(gray, 5)
+    _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel, iterations = 2)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_ERODE, kernel, iterations = 2)
+
+    thresh = cv2.resize(thresh, (32,32), interpolation = cv2.INTER_AREA)
+
+    x = thresh.reshape(-1, 32, 32, 1) / 255.0
+
+    print(Labels[np.argmax(model.predict([x]))])
