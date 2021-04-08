bff03ba9ea8889cf228541f77e154ce5099dba7d
Milind Prajapat <milindprajapat@gmail.com>
2021-04-08 23:29:33 +0530

    Update

--- Evaluate_Model.py
+++ Evaluate_Model.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import Split_Words
+import numpy as np
 import pandas as pd
 import Split_Characters
 import Predict_Characters
@@ -21,29 +22,16 @@ Label_Dict = {0: 'क', 1: 'ख', 2: 'ग', 3: 'घ', 4: 'ङ',
 
 df = pd.read_csv(os.path.join("Test", "Reference.csv"))
 
-for Image_Path in df.iloc[:,0]:
-    Words = Split_Words.Split(cv2.imread(Image_Path))
-    Word_Characters = Split_Characters.Split(Words)
-    Predictions = Predict_Characters.Predict(Word_Characters)
+Characters = [[cv2.imread(img) for img in df.iloc[:,0]]]
+y_pred = np.array(Predict_Characters.Predict(Characters)).reshape(-1).tolist()
 
-    for Prediction in Predictions:
-        Word = ""
-        for class_id in Prediction:
-            Word += Label_Dict[class_id]
-        print(Word, end = " ")
-    print("")
+y_true = df.iloc[:, 1].values.tolist()
 
-##Characters = [cv2.imread(img) for img in df.iloc[:,0]]
-##y_pred = Predict_Characters.Predict(Characters)
-##
-##y_true = df.iloc[:, 1].values.to_list()
-##
-##acc = accuracy_score(y_true, y_pred)
-##print("Accuracy on Test Data :", "{:.4%}".format(acc))
-##
-##y_pred = [Label_Dict[class_id] for class_id in np.argmax(y_pred, axis = 1)]
-##y_true = [Label_Dict[class_id] for class_id in np.argmax(y_true, axis = 1)]
-##
-##Results = pd.DataFrame(list(zip(df.iloc[:, 0], y_true, y_pred)), columns = ["File_Name", "Actual", "Prediction"])
-##Results.to_csv(os.path.join("Test", "Final_Results.csv"), index = False)
-##
+acc = accuracy_score(y_true, y_pred)
+print("Accuracy on Test Data :", "{:.4%}".format(acc))
+
+y_pred = [Label_Dict[class_id] for class_id in y_pred]
+y_true = [Label_Dict[class_id] for class_id in y_true]
+
+Results = pd.DataFrame(list(zip(df.iloc[:, 0], y_true, y_pred)), columns = ["File_Name", "Actual", "Prediction"])
+Results.to_csv(os.path.join("Test", "Final_Results.csv"), index = False)
