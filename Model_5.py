8fa5fff5d41f75946819f4fa4c61f2ece36d7d8b
milind-prajapat <milindprajapat@gmail.com>
2021-05-25 13:36:02 +0530

    Update Model_5

--- Model_5.py
+++ Model_5.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger, EarlyStopping
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 trainDataGen = ImageDataGenerator(rotation_range = 5,
                                   width_shift_range = 0.1,
@@ -38,8 +38,6 @@ model.add(Conv2D(64, (3, 3), activation = 'relu', strides = (1, 1), padding = 'v
 model.add(MaxPooling2D((2, 2), strides = (2, 2), padding = 'same'))
 
 model.add(Conv2D(64, (3, 3), activation = 'relu',strides = (1, 1), padding = 'valid'))
-model.add(BatchNormalization())
-
 model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'valid'))
 model.add(MaxPooling2D((2, 2), strides = (2, 2), padding = 'same'))
 
@@ -50,21 +48,21 @@ model.add(MaxPooling2D((2, 2), strides = (2, 2), padding = 'same'))
 model.add(Flatten())
 
 model.add(Dense(256, activation = 'relu'))
-model.add(Dropout(0.3))
+model.add(Dropout(0.2))
 
 model.add(Dense(128, activation = 'relu'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.1))
 
 model.add(Dense(49, activation = 'softmax'))
 
-model.compile(optimizer = Adam(lr = 1e-3, decay = 1e-5), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer = Adam(learning_rate = 1e-3, decay = 1e-5), loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 if not os.path.isdir('Model_5'):
     os.mkdir('Model_5')
 
 callbacks = [ReduceLROnPlateau(monitor = 'val_loss', factor = 0.1,
                               patience = 7, min_lr = 1e-5),
-             EarlyStopping(patience = 9, # Patience should be larger than the one in ReduceLROnPlateau
+             EarlyStopping(monitor = 'val_loss', patience = 9, # Patience should be larger than the one in ReduceLROnPlateau
                           min_delta = 1e-5),
              CSVLogger(os.path.join('Model_5', 'training.log'), append = True),
              ModelCheckpoint(os.path.join('Model_5', 'backup_last_model.hdf5')),
